a607f407a1f91322d0b643cf6ebf2b690f24cf9d
xurshidjumaqulov81-art <xurshidjumaqulov81@gmail.com>
2026-07-06 22:51:51 +0300

    Add RegisterStates and AdminStates classes

---
+++ states.py
@@ -0,0 +1,19 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class RegisterStates(StatesGroup):
+    full_name = State()
+    xj_id = State()
+    phone = State()
+    gender = State()
+    region = State()
+    confirm = State()
+
+
+class AdminStates(StatesGroup):
+    set_limit = State()
+    broadcast = State()
+    search = State()
+    private_message = State()
+    add_admin = State()
+    remove_admin = State()
