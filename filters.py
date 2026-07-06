c8e563a3e7f3b1a1bb37cd47753181888f74fb96
xurshidjumaqulov81-art <xurshidjumaqulov81@gmail.com>
2026-07-06 23:18:51 +0300

    Refactor AdminFilter to streamline admin checks
    Simplify AdminFilter logic by combining conditions.

--- filters.py
+++ filters.py
@@ -1,18 +1,10 @@
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
-
 from config import ADMIN_IDS
 from database import is_db_admin
-
 
 class AdminFilter(BaseFilter):
     async def __call__(self, message: Message) -> bool:
         telegram_id = message.from_user.id
+        return telegram_id in ADMIN_IDS or await is_db_admin(telegram_id)
 
-        if telegram_id in ADMIN_IDS:
-            return True
-
-        if await is_db_admin(telegram_id):
-            return True
-
-        return False
